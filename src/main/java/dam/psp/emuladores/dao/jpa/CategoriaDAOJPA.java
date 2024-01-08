01c891ce519e45a958a0cd010c09687ddae00d12
Hugossj3 <gdhugo666@gmail.com>
2024-01-08 12:50:37 +0100

    Puede que no funcione un metodo de CategoriaDAOJPA

--- src/main/java/dam/psp/emuladores/dao/jpa/CategoriaDAOJPA.java
+++ src/main/java/dam/psp/emuladores/dao/jpa/CategoriaDAOJPA.java
@@ -1,18 +1,30 @@
 package dam.psp.emuladores.dao.jpa;
 
 import dam.psp.emuladores.dao.CategoriaDAO;
+import dam.psp.emuladores.gestores.GestorEntityManager;
 import dam.psp.emuladores.modelo.Categoria;
+import dam.psp.emuladores.modelo.jpa.CategoriaJPA;
+import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 public class CategoriaDAOJPA implements CategoriaDAO {
     @Override
     public Categoria nuevaCategoria(String n) {
-        throw new UnsupportedOperationException("Método sin programar");
+        Categoria c;
+        try {
+            CategoriaJPA cj=new CategoriaJPA();
+            cj.setNombre(n);
+            c= (Categoria) cj;
+            EntityManager em=GestorEntityManager.getINSTANCIA().getEntityManager();
+        }catch (Exception e){
+            return  null;
+        }
+        return c;
     }
 
     @Override
-    public <T extends Categoria> List<T> getCategorias() {
+    public List<CategoriaJPA> getCategorias() {
         throw new UnsupportedOperationException("Método sin programar");
     }
 }
