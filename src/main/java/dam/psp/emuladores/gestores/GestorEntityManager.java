e4635603f7a42eec78cf5e26a0bf320097729671
Hugossj3 <gdhugo666@gmail.com>
2024-01-08 10:55:57 +0100

    abc

--- src/main/java/dam/psp/emuladores/gestores/GestorEntityManager.java
+++ src/main/java/dam/psp/emuladores/gestores/GestorEntityManager.java
@@ -1,7 +1,20 @@
 package dam.psp.emuladores.gestores;
 
+import jakarta.persistence.EntityManager;
+
 public class GestorEntityManager {
     private static GestorEntityManager INSTANCIA;
+    private EntityManager entityManager;
+    private GestorEntityManager(){
+
+    }
+    public EntityManager getEntityManager(){
+        throw new UnsupportedOperationException();
+    }
+    public static GestorEntityManager getINSTANCIA(){
+        throw new UnsupportedOperationException();
+    }
 
 }
 //static a la instancia
+//
