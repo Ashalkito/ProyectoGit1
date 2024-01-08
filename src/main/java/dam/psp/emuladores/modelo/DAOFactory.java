43b2fc5a196a5f02df3efc752dadff7a52dcbc52
aliherrerasz <aliherrera1505@gmail.com>
2024-01-08 11:12:40 +0100

    DAOFactory sin programar

--- src/main/java/dam/psp/emuladores/modelo/DAOFactory.java
+++ src/main/java/dam/psp/emuladores/modelo/DAOFactory.java
@@ -1,16 +1,21 @@
 package dam.psp.emuladores.modelo;
 
+import dam.psp.emuladores.dao.CategoriaDAO;
+import dam.psp.emuladores.dao.EmuladorDAO;
+import dam.psp.emuladores.dao.SistemaDAO;
+import dam.psp.emuladores.dao.VideojuegoDAO;
+
 public class DAOFactory {
     public static SistemaDAO getSistemaDAO(){
-
+        throw new UnsupportedOperationException("Sin programar");
     }
     public static EmuladorDAO getEmuladorDAO(){
-
+        throw new UnsupportedOperationException("Sin programar");
     }
     public static VideojuegoDAO getVideojuegoDAO(){
-
+        throw new UnsupportedOperationException("Sin programar");
     }
     public static CategoriaDAO getCategoriaDAO(){
-
+        throw new UnsupportedOperationException("Sin programar");
     }
 }
