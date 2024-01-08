29b84a9c2b5ae161c69e903d985032f57792898c
Hugossj3 <gdhugo666@gmail.com>
2024-01-08 12:03:57 +0100

    abc

--- src/main/java/dam/psp/emuladores/gestores/GestorExecutorService.java
+++ src/main/java/dam/psp/emuladores/gestores/GestorExecutorService.java
@@ -1,15 +1,25 @@
 package dam.psp.emuladores.gestores;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GestorExecutorService {
     private static GestorExecutorService INSTANCIA;
     private ExecutorService executorService;
-    private GestorExecutorService(){}
+    private GestorExecutorService(){
+        executorService= Executors.newCachedThreadPool();
+    }
     public ExecutorService getExecutorService(){
-        throw new UnsupportedOperationException("");
+        return executorService;
     }
     public static GestorExecutorService getINSTANCIA(){
-        throw new UnsupportedOperationException("");
+        GestorExecutorService ges;
+        if(INSTANCIA==null){
+            ges=new GestorExecutorService();
+        }else{
+            ges=INSTANCIA;
+        }
+        INSTANCIA=ges;
+        return INSTANCIA;
     }
 }
