597963e0ca0a81a8db0abc9b3a9234afdbd94135
aliherrerasz <aliherrera1505@gmail.com>
2024-01-08 12:29:22 +0100

    Emulador: método abrir en proceso

--- src/main/java/dam/psp/emuladores/modelo/Emulador.java
+++ src/main/java/dam/psp/emuladores/modelo/Emulador.java
@@ -1,5 +1,10 @@
 package dam.psp.emuladores.modelo;
 
+import java.awt.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public interface Emulador {
     public int getId();
     public String getNombre();
@@ -7,6 +12,13 @@ public interface Emulador {
     public String getLineaComandos();
     public Sistema getSistema();
     public default boolean abrir(Videojuego v, boolean grabarVideo){
-        throw new UnsupportedOperationException(("Sin programar"));
+        String archivoSalida = v.getNombre()+ LocalDate.now()+ LocalTime.now()+".mp4";
+        String comandoGrabar = "ffmpeg -s "+Toolkit.getDefaultToolkit().getScreenResolution()+"-framerate 25 -f x11grab -i :0.0 "+archivoSalida;
+
+        try {
+            Process proceso = new ProcessBuilder(comandoGrabar).start();
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
     }
 }
