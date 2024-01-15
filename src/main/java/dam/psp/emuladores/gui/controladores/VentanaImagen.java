7f899637098715ebb421882d688e2c45fb011499
Jose Luis IESHLANZ <joluba2002@gmail.com>
2024-01-15 10:33:45 +0100

    He creado la ventana de imagen del juego junto a su controlador

---
+++ src/main/java/dam/psp/emuladores/gui/controladores/VentanaImagen.java
@@ -0,0 +1,35 @@
+package dam.psp.emuladores.gui.controladores;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VentanaImagen implements Initializable {
+
+    @FXML
+    private Button btnJugar;
+
+    @FXML
+    private CheckBox chkGrabar;
+
+    @FXML
+    private ComboBox<?> cmbEmulador;
+
+    @FXML
+    private ImageView imgImagen;
+
+    @FXML
+    private Label lblTitulo1;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
