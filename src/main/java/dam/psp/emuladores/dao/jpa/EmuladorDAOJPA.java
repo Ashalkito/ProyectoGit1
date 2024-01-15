74cb54ce847c9662ef68289c2fdf266c55df47d7
mmarm09 <marina09mart@gmail.com>
2024-01-15 09:01:37 +0100

    EmuladorDAOJPA terminado

--- src/main/java/dam/psp/emuladores/dao/jpa/EmuladorDAOJPA.java
+++ src/main/java/dam/psp/emuladores/dao/jpa/EmuladorDAOJPA.java
@@ -1,25 +1,45 @@
 package dam.psp.emuladores.dao.jpa;
 
 import dam.psp.emuladores.dao.EmuladorDAO;
+import dam.psp.emuladores.gestores.GestorEntityManager;
 import dam.psp.emuladores.modelo.Emulador;
 import dam.psp.emuladores.modelo.Sistema;
 import dam.psp.emuladores.modelo.jpa.EmuladorJPA;
 import dam.psp.emuladores.modelo.jpa.SistemaJPA;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmuladorDAOJPA implements EmuladorDAO {
 
     @Override
     public Emulador nuevoEmulador(String nombre, String ruta, String lc, Sistema s){
-        /*if(s instanceof SistemaJPA){
+        EmuladorJPA e=null;
+        if(s instanceof SistemaJPA){
+            GestorEntityManager gem=GestorEntityManager.getINSTANCIA();
+            e=new EmuladorJPA();
+            e.setNombre(nombre);
+            e.setRuta(ruta);
+            e.setLineaComandos(lc);
+            e.setSistema((SistemaJPA) s);
 
-        }*/
-        throw new UnsupportedOperationException("Método sin programar");
+            gem.getEntityManager().getTransaction().begin();
+            gem.getEntityManager().persist(e);
+            gem.getEntityManager().getTransaction().commit();
+        }else{
+            throw new IllegalArgumentException("El sistema no puede ser guardado en la base de datos");
+        }
+        return e;
     }
     @Override
-    public <T extends Emulador> List<T> getEmuladores(Sistema s) {
-        throw new UnsupportedOperationException("Método sin programar");
+    public List<EmuladorJPA> getEmuladores(Sistema s) {
+        List<EmuladorJPA> lista=new ArrayList<>();
+        GestorEntityManager gem=GestorEntityManager.getINSTANCIA();
+        lista=gem.getEntityManager()
+                .createQuery("Select emu from EmuladorJPA emu", EmuladorJPA.class)
+                .getResultList();
+
+        return lista;
     }
 
 }
