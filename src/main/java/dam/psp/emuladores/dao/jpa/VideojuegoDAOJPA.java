fb9fccdf11473a952379febdd1baf34cd9aa7f52
Rocio440 <cabrerizorocio@gmail.com>
2024-01-15 09:18:03 +0100

    a

--- src/main/java/dam/psp/emuladores/dao/jpa/VideojuegoDAOJPA.java
+++ src/main/java/dam/psp/emuladores/dao/jpa/VideojuegoDAOJPA.java
@@ -2,6 +2,7 @@ package dam.psp.emuladores.dao.jpa;
 
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import dam.psp.emuladores.dao.VideojuegoDAO;
+import dam.psp.emuladores.gestores.GestorEntityManager;
 import dam.psp.emuladores.modelo.Categoria;
 import dam.psp.emuladores.modelo.Sistema;
 import dam.psp.emuladores.modelo.Videojuego;
@@ -9,6 +10,7 @@ import dam.psp.emuladores.modelo.jpa.CategoriaJPA;
 import dam.psp.emuladores.modelo.jpa.SistemaJPA;
 import dam.psp.emuladores.modelo.jpa.VideojuegoJPA;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,16 +19,33 @@ public class VideojuegoDAOJPA implements VideojuegoDAO {
     }
     @Override
     public Videojuego nuevoVideojuego(String nombre, Sistema s, String rutaFoto, List<Categoria> c) {
-        for(Categoria i:c ){
-        if(s instanceof SistemaJPA && i instanceof CategoriaJPA) {
-            return nuevoVideojuego(nombre, s, rutaFoto, c);
-        }else{
-            throw new IllegalArgumentException("error");
+        GestorEntityManager gm = GestorEntityManager.getINSTANCIA();
+       VideojuegoJPA video = null;
+        for(Categoria i:c ) {
+            if (s instanceof SistemaJPA && i instanceof CategoriaJPA) {
+                video = (VideojuegoJPA) nuevoVideojuego(nombre, s, rutaFoto, c);
+            } else {
+                throw new IllegalArgumentException("error");
+            }
+            try {
+                gm.getEntityManager().getTransaction().begin();
+                gm.getEntityManager().persist(video);
+                gm.getEntityManager().getTransaction().commit();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
+        return video;
     }
 
     @Override
     public List <VideojuegoJPA> getVideojuegos(String patron, Sistema s, Categoria c) {
-        throw new UnsupportedOperationException("Método sin programar");
+        List<VideojuegoJPA> listavideo = new ArrayList<>();
+
+        GestorEntityManager gm = GestorEntityManager.getINSTANCIA();
+        VideojuegoJPA videoencontrar = new VideojuegoJPA();
+        gm.getEntityManager().find(VideojuegoDAO.class,videoencontrar);
+
+        return listavideo;
     }
 }
