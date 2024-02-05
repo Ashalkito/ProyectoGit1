6e5fa5acf98950418ebb98bab52480015d20a9b3
mmarm09 <marina04mart@gmail.com>
2024-02-05 09:21:35 +0100

    Ventana emergente sistema y categoria

--- src/main/java/dam/psp/emuladores/gui/controladores/VentanaCategoria.java
+++ src/main/java/dam/psp/emuladores/gui/controladores/VentanaCategoria.java
@@ -2,14 +2,20 @@ package dam.psp.emuladores.gui.controladores;
 
 import dam.psp.emuladores.dao.CategoriaDAO;
 import dam.psp.emuladores.dao.jpa.CategoriaDAOJPA;
+import dam.psp.emuladores.dao.jpa.SistemaDAOJPA;
 import dam.psp.emuladores.modelo.Categoria;
+import dam.psp.emuladores.modelo.Sistema;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class VentanaCategoria extends ControladorSecundario implements Initializable {
@@ -26,9 +32,25 @@ public class VentanaCategoria extends ControladorSecundario implements Initializ
     }
 
     public void btnAceptarCategoria(ActionEvent actionEvent) {
-        Categoria c= new CategoriaDAOJPA().nuevaCategoria(txfNombre.getText());
-        this.getCp().recargarVentana();
-        this.getStage().close();
+        if(txfNombre.getText().isEmpty()){
+            Alert alertaError=new Alert(Alert.AlertType.ERROR);
+            alertaError.setTitle("Error");
+            alertaError.setHeaderText("Error al introducir una nueva categoria");
+            alertaError.setContentText("No se han introducido los datos necesarios");
+            alertaError.showAndWait();
+        }else{
+            Categoria c= new CategoriaDAOJPA().nuevaCategoria(txfNombre.getText());
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+            alerta.setTitle(new String("Nueva inserción".getBytes(), Charset.forName("UTF-8")));
+            alerta.setHeaderText(new String("¿Introducir nueva categoria?".getBytes(), Charset.forName("UTF-8")));
+            alerta.setContentText(new String("Se creará una nueva entrada en la base de datos.".getBytes(), Charset.forName("UTF-8")));
+
+            Optional<ButtonType> resultado = alerta.showAndWait();
+            if (resultado.isPresent() && resultado.get() == javafx.scene.control.ButtonType.OK && this.getStage()!=null) {
+                this.getCp().recargarVentana();
+                this.getStage().close();
+            }
+        }
     }
 }
 
--- src/main/java/dam/psp/emuladores/gui/controladores/VentanaSistema.java
+++ src/main/java/dam/psp/emuladores/gui/controladores/VentanaSistema.java
@@ -2,14 +2,20 @@ package dam.psp.emuladores.gui.controladores;
 
 import dam.psp.emuladores.dao.SistemaDAO;
 import dam.psp.emuladores.dao.jpa.SistemaDAOJPA;
+import dam.psp.emuladores.dao.jpa.VideojuegoDAOJPA;
 import dam.psp.emuladores.modelo.Sistema;
+import dam.psp.emuladores.modelo.Videojuego;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class VentanaSistema extends ControladorSecundario implements Initializable {
@@ -26,9 +32,25 @@ public class VentanaSistema extends ControladorSecundario implements Initializab
     }
 
     public void btnAceptarSistema(ActionEvent actionEvent) {
-        Sistema sistema = new SistemaDAOJPA().nuevoSistema(txfNombre.getText());
-        this.getCp().recargarVentana();
-        this.getStage().close();
+        if(txfNombre.getText().isEmpty()){
+            Alert alertaError=new Alert(Alert.AlertType.ERROR);
+            alertaError.setTitle("Error");
+            alertaError.setHeaderText("Error al introducir un nuevo sistema");
+            alertaError.setContentText("No se han introducido los datos necesarios");
+            alertaError.showAndWait();
+        }else{
+            Sistema sistema = new SistemaDAOJPA().nuevoSistema(txfNombre.getText());
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+            alerta.setTitle(new String("Nueva inserción".getBytes(), Charset.forName("UTF-8")));
+            alerta.setHeaderText(new String("¿Introducir nuevo sistema?".getBytes(), Charset.forName("UTF-8")));
+            alerta.setContentText(new String("Se creará una nueva entrada en la base de datos.".getBytes(), Charset.forName("UTF-8")));
+
+            Optional<ButtonType> resultado = alerta.showAndWait();
+            if (resultado.isPresent() && resultado.get() == javafx.scene.control.ButtonType.OK && this.getStage()!=null) {
+                this.getCp().recargarVentana();
+                this.getStage().close();
+            }
+        }
     }
 }
 
