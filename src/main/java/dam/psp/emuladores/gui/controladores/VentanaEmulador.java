59c74e87d1ee58a9b978b2f8bf705336855470d6
mmarm09 <marina04mart@gmail.com>
2024-02-05 09:30:53 +0100

    Ventana emergente emuladores

--- src/main/java/dam/psp/emuladores/gui/controladores/VentanaEmulador.java
+++ src/main/java/dam/psp/emuladores/gui/controladores/VentanaEmulador.java
@@ -1,25 +1,27 @@
 package dam.psp.emuladores.gui.controladores;
 
 import dam.psp.emuladores.dao.jpa.EmuladorDAOJPA;
+import dam.psp.emuladores.dao.jpa.VideojuegoDAOJPA;
 import dam.psp.emuladores.gestores.GestorEntityManager;
 import dam.psp.emuladores.modelo.Emulador;
 import dam.psp.emuladores.modelo.Sistema;
+import dam.psp.emuladores.modelo.Videojuego;
 import dam.psp.emuladores.modelo.jpa.SistemaJPA;
 import dam.psp.emuladores.modelo.jpa.VideojuegoJPA;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class VentanaEmulador extends ControladorSecundario implements Initializable {
@@ -64,11 +66,26 @@ public class VentanaEmulador extends ControladorSecundario implements Initializa
     }
 
     public void btnAceptarEmulador(ActionEvent actionEvent) {
-        Emulador emulador = new EmuladorDAOJPA().nuevoEmulador(txfNombre.getText(),txfRutaInstalacion.getText(),txfLineaComandos
-                .getText(),chbSistemas.getValue());
-        this.getCp().recargarVentana();
-        this.getStage().close();
+        if(chbSistemas.getValue()==null || txfLineaComandos.getText().isEmpty() ||
+                txfNombre.getText().isEmpty() || txfRutaInstalacion.getText().isEmpty()){
+            Alert alertaError=new Alert(Alert.AlertType.ERROR);
+            alertaError.setTitle("Error");
+            alertaError.setHeaderText("Error al introducir un nuevo emulador");
+            alertaError.setContentText("No se han introducido los datos necesarios");
+            alertaError.showAndWait();
+        }else{
+            Emulador emulador = new EmuladorDAOJPA().nuevoEmulador(txfNombre.getText(),txfRutaInstalacion.getText(),txfLineaComandos
+                    .getText(),chbSistemas.getValue());
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+            alerta.setTitle(new String("Nueva inserción".getBytes(), Charset.forName("UTF-8")));
+            alerta.setHeaderText(new String("¿Introducir nuevo emulador?".getBytes(), Charset.forName("UTF-8")));
+            alerta.setContentText(new String("Se creará una nueva entrada en la base de datos.".getBytes(), Charset.forName("UTF-8")));
+
+            Optional<ButtonType> resultado = alerta.showAndWait();
+            if (resultado.isPresent() && resultado.get() == javafx.scene.control.ButtonType.OK && this.getStage()!=null) {
+                this.getCp().recargarVentana();
+                this.getStage().close();
+            }
+        }
     }
-
 }
-
