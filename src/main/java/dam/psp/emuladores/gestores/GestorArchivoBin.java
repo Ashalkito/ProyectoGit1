620235a62b2163e0797722d51de57b20e1ddca4d
Rocio440 <cabrerizorocio@gmail.com>
2024-02-12 12:38:30 +0100

    realización de los gettes(int id)

--- src/main/java/dam/psp/emuladores/gestores/GestorArchivoBin.java
+++ src/main/java/dam/psp/emuladores/gestores/GestorArchivoBin.java
@@ -1,11 +1,15 @@
 package dam.psp.emuladores.gestores;
 
+import dam.psp.emuladores.dao.VideojuegoDAO;
+import dam.psp.emuladores.dao.bin.EmuladorDAOBin;
+import dam.psp.emuladores.modelo.Sistema;
 import dam.psp.emuladores.modelo.bin.CategoriaBin;
 import dam.psp.emuladores.modelo.bin.EmuladorBin;
 import dam.psp.emuladores.modelo.bin.SistemaBin;
 import dam.psp.emuladores.modelo.bin.VideojuegoBin;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestorArchivoBin {
@@ -15,7 +19,12 @@ public class GestorArchivoBin {
     private List<EmuladorBin> emuladores;
     private List<VideojuegoBin> videojuegos;
 
-    private GestorArchivoBin(){}
+    private GestorArchivoBin(){
+        categorias= new ArrayList<>();
+        sistemas= new ArrayList<>();
+        emuladores= new ArrayList<>();
+        videojuegos= new ArrayList<>();
+    }
 
     public static GestorArchivoBin getInstancia(){
         GestorArchivoBin gbin;
@@ -45,19 +54,43 @@ public class GestorArchivoBin {
     }
 
     public List<CategoriaBin> getCategorias(int id) {
-        throw new UnsupportedOperationException("sin programar");
+        List<CategoriaBin> listacategorias= new ArrayList<>();
+        for(CategoriaBin i:categorias){
+            if(i.getId()==id){
+                listacategorias.add(i);
+            }
+        }
+        return listacategorias;
     }
 
     public List<SistemaBin> getSistemas(int id) {
-        throw new UnsupportedOperationException("sin programar");
+        List<SistemaBin> listasistemas = new ArrayList<>();
+        for(SistemaBin i:sistemas){
+            if(i.getId()==id){
+                listasistemas.add(i);
+            }
+        }
+        return listasistemas;
     }
 
     public List<EmuladorBin> getEmuladores(int id) {
-        throw new UnsupportedOperationException("sin programar");
+        List<EmuladorBin> listaemuladores= new ArrayList<>();
+        for(EmuladorBin i: emuladores){
+            if(i.getId()==id){
+                listaemuladores.add(i);
+            }
+        }
+        return listaemuladores;
     }
 
     public List<VideojuegoBin> getVideojuegos(int id) {
-        throw new UnsupportedOperationException("sin programar");
+        List<VideojuegoBin> listavideojuegos= new ArrayList<>();
+        for(VideojuegoBin i: videojuegos){
+            if(i.getId()==id){
+                listavideojuegos.add(i);
+            }
+        }
+        return listavideojuegos;
     }
 
     public void guardar() throws IOException {
@@ -65,7 +98,6 @@ public class GestorArchivoBin {
     }
     public void cargar() throws IOException{
         throw new UnsupportedOperationException("sin programar");
-
     }
 
 }
