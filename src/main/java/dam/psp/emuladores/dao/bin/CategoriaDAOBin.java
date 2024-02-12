7bd1705752e754a448bde0e4d8a416f2ecc70ebe
aliherrerasz <aliherrera1505@gmail.com>
2024-02-12 09:44:51 +0100

    CategoriaDAOBin (creo que es así jaja)

--- src/main/java/dam/psp/emuladores/dao/bin/CategoriaDAOBin.java
+++ src/main/java/dam/psp/emuladores/dao/bin/CategoriaDAOBin.java
@@ -1,18 +1,26 @@
 package dam.psp.emuladores.dao.bin;
 
 import dam.psp.emuladores.dao.CategoriaDAO;
+import dam.psp.emuladores.gestores.GestorArchivoBin;
 import dam.psp.emuladores.modelo.Categoria;
+import dam.psp.emuladores.modelo.bin.CategoriaBin;
+import dam.psp.emuladores.modelo.bin.SistemaBin;
 
 import java.util.List;
 
 public class CategoriaDAOBin implements CategoriaDAO {
     @Override
     public Categoria nuevaCategoria(String n) {
-        return null;
+        GestorArchivoBin gab = GestorArchivoBin.getInstancia();
+        List<CategoriaBin> lista = gab.getCategorias();
+        CategoriaBin ultimo = lista.get(lista.size()-1);
+        CategoriaBin c = new CategoriaBin(ultimo.getId()+1, n, gab.getVideojuegos());
+        lista.add(c);
+        return c;
     }
 
     @Override
-    public <T extends Categoria> List<T> getCategorias() {
-        return null;
+    public List<CategoriaBin> getCategorias() {
+        return GestorArchivoBin.getInstancia().getCategorias();
     }
 }
